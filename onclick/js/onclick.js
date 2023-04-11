44d85f965e071db7511d0fb06189c9e1d62f18a8
Abhay Sharma <abhay.shar98@gmail.com>
2023-04-11 21:42:53 +0530

    messages changes

--- onclick/js/onclick.js
+++ onclick/js/onclick.js
@@ -1,24 +1,24 @@
 let goodmorning=(message, bgcolor, color)=>
 {
-  let tag=document.querySelector("#one");
+  let tag=document.querySelector("#messages");
   tag.innerText=message;
   tag.style.backgroundColor=bgcolor;
   tag.style.color=color;
 }
-let first=document.querySelector("#one1");
+let first=document.querySelector("#morning");
 first.addEventListener('click',function(){
 goodmorning('good morning','red','black');
 });
 
-let second=document.querySelector("#one2");
+let second=document.querySelector("#afternoon");
 second.addEventListener('click',function(){
 goodmorning('Good afternoon','black','red');
 });
-let third=document.querySelector("#one3");
+let third=document.querySelector("#evening");
 third.addEventListener('click',function(){
 goodmorning('good evening','red','yellow');
 });
-let four=document.querySelector("#one4");
+let four=document.querySelector("#night");
 four.addEventListener('click',function(){
 goodmorning('good night','purple','white');
 });
